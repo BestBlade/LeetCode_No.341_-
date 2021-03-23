5bbafe09d5641cba56f3608ee3ddadeb5bab0d41
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-03-23 10:32:29 +0800

    Add files via upload

---
+++ leetcode_no341_扁平化嵌套列表迭代器_w1.cpp
@@ -0,0 +1,113 @@
+﻿/* ------------------------------------------------------------------|
+给你一个嵌套的整型列表。请你设计一个迭代器，使其能够遍历这个整型列表中的所有整数。
+
+列表中的每一项或者为一个整数，或者是另一个列表。其中列表的元素也可能是整数或是其他列表。
+
+ 
+
+示例 1:
+
+输入: [[1,1],2,[1,1]]
+输出: [1,1,2,1,1]
+解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,1,2,1,1]。
+示例 2:
+
+输入: [1,[4,[6]]]
+输出: [1,4,6]
+解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,4,6]。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/flatten-nested-list-iterator
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	双指针法
+*
+*	执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：8.1 MB, 在所有 C++ 提交中击败了81.94%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+//别人写好的函数
+class NestedInteger {
+public:
+    // Return true if this NestedInteger holds a single integer, rather than a nested list.
+    bool isInteger() const;
+
+    // Return the single integer that this NestedInteger holds, if it holds a single integer
+    // The result is undefined if this NestedInteger holds a nested list
+    int getInteger() const;
+
+    // Return the nested list that this NestedInteger holds, if it holds a nested list
+    // The result is undefined if this NestedInteger holds a single integer
+    const vector<NestedInteger>& getList() const;
+};
+
+//完成下面的空
+class NestedIterator {
+private:
+    stack< pair<vector<NestedInteger>::iterator, vector<NestedInteger>::iterator> > st;
+public:
+    NestedIterator(vector<NestedInteger>& nestedList) {
+        st.emplace(nestedList.begin(), nestedList.end());
+    }
+
+    int next() {
+        return st.top().first++->getInteger();
+    }
+
+    bool hasNext() {
+        while (!st.empty()) {
+            auto& p =  st.top();
+            if (p.first == p.second) {
+                st.pop();
+                continue;
+            }
+            if (p.first->isInteger()) {
+                return true;
+            }
+            auto& l = p.first++->getList();
+            st.emplace(l.begin(), l.end());
+        }
+        return false;
+    }
+};
